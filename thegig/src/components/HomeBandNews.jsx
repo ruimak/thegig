8b27af40b5d8ade8a40229ab695c13251b07d0b8
danny087 <dflask13@gmail.com>
2019-03-06 18:08:22 +0000

    homebandnews

---
+++ thegig/src/components/HomeBandNews.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+
+import {getAllBandNews} from '../api'
+
+export default class HomeBandNews extends Component {
+  
+  state = {
+      bandNews : []
+  }
+
+  componentDidMount() {
+      getAllBandNews().then(bandNews => {
+          console.log(bandNews.data.articles,'kkkkk')
+          this.setState({bandNews : bandNews.data.articles})
+      })
+  }
+    render() {
+        console.log(this.state,'hello')
+    return (
+      <div>
+          
+          {this.state.bandNews !== null ? this.state.bandNews.map(news => {
+        return (
+        <div>
+        {news.content}
+        <a href={news.url}>{news.url}</a>
+        
+        </div>
+        )
+        }) : 'no news to display'}
+          
+        
+      </div>
+    )
+  }
+}
