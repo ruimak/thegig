347b94dbfbf0003432af7030b15250622e777432
Rui Mak <rui.mak@hotmail.com>
2019-03-15 15:58:25 +0000

    mybands is done

--- thegig/src/components/defaultPage/Mybands.jsx
+++ thegig/src/components/defaultPage/Mybands.jsx
@@ -1,16 +1,42 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import firebase from "../../firebase.js";
 
-const MyBands = (props) => {
+export default class MyBands extends Component {
 
-  return(
+   state = {
+      bandsFollowed : []
+  }
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log(user.uid, 'THIS IS THE USER ID I WANT');
+        firebase
+          .database()
+          .ref()
+          .once("value")
+          .then(
+            function(userData) {
+              const bands = userData.val().users[user.uid]
+                .bands
+                ? Object.values(
+                    userData.val().users[user.uid].bands
+                  )
+                : [];
+                console.log(bands, 'THESE ARE THE BANDS THAT YOU FOLLOW')
+              this.setState({ bandsFollowed: bands });
+            }.bind(this)
+          );
+      }
+    });
+  }
+ render(){
+   return(
       <div>
-        
-      {props.myBands && props.myBands.map((band)=>{
+      {this.state.bandsFollowed.map((band)=>{
         return <div><Link to={`/artist/${band}/news`}>{band}</Link><br/></div>
       })}
       </div>
-  )
+  )} 
 } 
 
-export default MyBands
