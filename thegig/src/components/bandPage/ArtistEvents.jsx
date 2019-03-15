0923eba179886b3f747de5ddb081e69629352945
Rui Mak <rui.mak@hotmail.com>
2019-03-15 19:33:22 +0000

    artist events is organized

--- thegig/src/components/bandPage/ArtistEvents.jsx
+++ thegig/src/components/bandPage/ArtistEvents.jsx
@@ -1,34 +1,42 @@
-import React, { Component } from 'react'
-import {getArtistEvent} from '../../api'
+import React, { Component } from "react";
+import { getArtistEvent } from "../../api";
 
 export default class ArtistEvents extends Component {
   state = {
-      eventsInfo : null
+    eventsInfo: null
+  };
+  componentDidMount() {
+    getArtistEvent(this.props.params.band).then(events => {
+      this.setState({ eventsInfo: events.data._embedded.events });
+    });
   }
-  componentDidMount(){
-      getArtistEvent(this.props.params.band)
-      .then(events=> {
-          this.setState({eventsInfo: events.data._embedded.events})
-      })
-  }
-    render() {
-    return (
-      <div>
-
-{
-          (this.state.eventsInfo!== null) ? this.state.eventsInfo.map(event=>{
-              return <div>{event.name}</div>
-          }) : "no events to show yet"
-        
-        }
-
-      </div>
-    )
+  render() {
+    console.log(this.state.eventsInfo);
+    if (!this.state.eventsInfo) {
+      return <div>{"No events to show."}</div>;
+    } else
+      return (
+        <div>
+          {this.state.eventsInfo.map(event => {
+            return (
+              <div>
+                <div>{event.name}</div>
+                <img src={event.images[0].url} />
+                <div>
+                  {"Locale: " +
+                    event._embedded.venues[0].country.name +
+                    ", " +
+                    event._embedded.venues[0].city.name}
+                </div>
+                <div>{"Venue: " + event._embedded.venues[0].name}</div>
+                <div>{"Date: " + event.dates.start.localDate}</div>
+                <br/>
+              </div>
+            );
+          })}
+        </div>
+      );
   }
 }
 
-
 // if there are no events its bugging out. Should be easy to fix with a condition
-
-
-
