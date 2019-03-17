a171939437f6069660f553152791a5f2778ad2e8
Rui Mak <rui.mak@hotmail.com>
2019-03-17 14:28:56 +0000

    rebuilt searchbar from scratch

--- thegig/src/components/NavBar.jsx
+++ thegig/src/components/NavBar.jsx
@@ -6,16 +6,16 @@ import { Link } from 'react-router-dom';
 class NavBar extends Component {
 
     render() {
-   const navBarComponent = <div >
+   const navBarComponent = <div className='Inline'>
       {this.props.params ? this.props.tabs.map(tab=>{
-          return <div> 
+          return <div className='Inline'> 
               <Link to={`/artist/${this.props.params.band}/${tab[0]}/`} className='NavBar'>
                   {tab[1]} 
                 </Link>
           </div>}) 
           : 
           this.props.tabs.map(tab=>{
-            return <div> 
+            return <div className='Inline'> 
                 <Link to={`/${tab[0]}`} className='NavBar'>
                     {tab[1]} 
                   </Link>
