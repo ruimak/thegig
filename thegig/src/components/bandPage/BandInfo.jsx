6bf9b36696e4762d0674f6e05d7794cd9ea71121
danny087 <dflask13@gmail.com>
2019-03-17 19:58:21 +0000

    styled band info

--- thegig/src/components/bandPage/BandInfo.jsx
+++ thegig/src/components/bandPage/BandInfo.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import { getBandInfo } from "../../api";
 import RelatedArtists from "./RelatedArtists";
+import {Tabs,Avatar,Typography,Grid} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import 'typeface-roboto';
 
-export default class BandInfo extends Component {
+
+
+
+
+
+class BandInfo extends Component {
   state = {
     bio: null
   };
@@ -12,14 +20,85 @@ export default class BandInfo extends Component {
       this.setState({ bio: bandInfo.data.artist });
     });
   }
+  
+  
+
   render() {
-    console.log(this.state.bio, "this is the bio");
+    console.log(this.props, "this is the bio");
+    // const { classes } = this.props.classes;
 if(!this.state.bio) {return null}
-else return <div>
-    <p>{this.state.bio.name}</p>
-<img src={this.state.bio.image[4]["#text"]}/>
-<div>{this.state.bio.bio.content}</div>
-<RelatedArtists artists={Object.values(this.state.bio.similar.artist)}/>
-</div> ;
+else return <div className={this.props.classes.wholeScreen}>
+  <div className='container'>
+  <Grid container
+  md={15}
+  // direction="row"
+  // justify="center"
+  // alignItems="center"
+>
+<Grid container md={8}>
+<Grid className={this.props.classes.titleAndAvatar} item md={4}> 
+
+<Avatar  direction="row"
+  justify="center"
+  alignItems="center"   className={this.props.classes.avatar}  src={this.state.bio.image[4]["#text"]} />
+</Grid>
+<Grid item md={4}> 
+<Typography  className={`${this.props.classes.header}`} >{this.state.bio.name}</Typography>
+
+</Grid>
+</Grid>
+
+
+<Grid item sm ={9}>
+<Typography  inline={true} className={this.props.classes.text}>{this.state.bio.bio.content}</Typography>
+</Grid>
+<Grid item sm={3}>
+<RelatedArtists  artists={Object.values(this.state.bio.similar.artist)}/>
+</Grid>
+</Grid> 
+ </div>
+ 
+</div>
+
   }
 }
+
+const styles = {
+  
+  container:{
+    paddingLeft:'20%',
+    paddingRight:'20%'
+  },
+  avatar : {
+    height:150,
+    width:150
+    
+  },
+  header : {
+    fontSize:'3.5em',
+    color: '#b288c0',
+    fontFamily: "'Lilita One', 'cursive'",
+    textColor:'grey',
+    paddingTop:'10%'
+  
+  },
+  text : {
+    fontFamily: "'Titillium Web', 'sans-serif'",
+    fontSize:'1.4em',
+    color:'#444',
+    lineHeight: 2
+  },
+  wholeScreen : {
+    paddingRight:'20%',
+    paddingLeft:'20%'
+  },
+  titleAndAvatar : {
+    paddingBottom : '8%'
+  }
+  
+  
+}
+console.log(styles,'these are the styles')
+
+
+export default withStyles(styles)(BandInfo);
--- thegig/src/components/bandPage/RelatedArtists.jsx
+++ thegig/src/components/bandPage/RelatedArtists.jsx
@@ -1,20 +1,47 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import {Tabs,Avatar,Typography,Grid} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
 
 
 const RelatedArtists = (props) => {
     return (
-        <div>{console.log(props.artists, 'these are the related artists')}
+        <div direction="column"
+        justify="center"
+        alignItems="center" className={props.classes.relatedArtists}>{console.log(props.artists, 'these are the related artists')}
              {props.artists && props.artists.map(
                  artist=>{
-                 return <div><img href={`/artist/${artist.name}/news`} src={artist.image[1]["#text"]}/><Link to={`/artist/${artist.name}/news`}>{artist.name}</Link><br/></div>}
+                 return <div className={props.classes.individualArtist}><Avatar className={props.classes.avatar} href={`/artist/${artist.name}/news`} src={artist.image[1]["#text"]}/><Link className={props.classes.link}  to={`/artist/${artist.name}/news`}>{artist.name}</Link><br/></div>}
              )}
         </div>
       )
 }
 
-export default RelatedArtists
+const styles = {
+    avatar : {
+        height:100,
+    width:100,
+    },
+    relatedArtists : {
+        paddingLeft:'100px'
+    },
+    individualArtist: {
+        paddingBottom : '25px'
+    },
+    link : {
+        fontFamily: "'Titillium Web', 'sans-serif'",
+        textDecoration: 'none',
+        marginLeft:'15px',
+        '&.active, &:hover, &.active:hover' : {
+            color: '#9a48d0'
+            
+        }
+        
+    }
+}
+
+export default withStyles(styles)(RelatedArtists)
 
 
 
