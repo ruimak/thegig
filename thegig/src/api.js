659f4dc58975294c5a0b7f421bdb03cd6e6831e8
Rui Mak <rui.mak@hotmail.com>
2019-03-27 05:14:32 +0000

    searchbar now has songs

--- thegig/src/api.js
+++ thegig/src/api.js
@@ -23,6 +23,12 @@ export const getBandSuggestions = bandName => {
   );
 };
 
+export const getSongSuggestions = song => {
+  return axios.get(
+    `http://ws.audioscrobbler.com/2.0/?method=track.search&track=${song}&api_key=${lastFmAPIkey}&format=json`
+  );
+};
+
 export const getBandInfo = bandName => {
   return axios.get(
     `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${bandName}&api_key=${lastFmAPIkey}&format=json`
